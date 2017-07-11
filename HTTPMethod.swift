1c69d7cee140393a565fb8abf1cd6bd62c64d2ca
toda <starfish719@gmail.com>
2017-07-12 00:52:26 +0900

    リクエストに関わる各種ファイルを追加

---
+++ HTTPMethod.swift
@@ -0,0 +1,19 @@
+//
+//  HTTPMethod.swift
+//  GitHubSearchRepository
+//
+//  Created by TodaChihaya on 2017/07/12.
+//  Copyright © 2017年 TodaChihaya. All rights reserved.
+//
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case head = "HEAD"
+    case delete = "DELETE"
+    case patch = "PATCH"
+    case trace = "TRACE"
+    case options = "OPTIONS"
+    case connect = "CONNECT"
+}
